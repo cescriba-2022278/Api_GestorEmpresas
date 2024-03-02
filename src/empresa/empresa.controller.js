de44060b3bbcf83934887a80c4c519bbef8a2fb1
cescriba <cescriba-2022278@kinal.edu.gt>
2024-03-02 01:13:57 -0600

    Proyecto terminado

--- src/empresa/empresa.controller.js
+++ src/empresa/empresa.controller.js
@@ -1,4 +1,7 @@
 import { response, request } from "express";
+import path from 'path';
+import ExcelJS from 'exceljs';
+import fs from 'fs';
 import Empresa from './empresa.model.js';
 
 export const getEmpresas = async (req, res) => {
@@ -41,8 +44,65 @@ export const crearEmpresa = async (req = request, res = response) => {
         const { nombre, nivelImpacto, yearsTrayectoria, categoriaEmpresarial } = req.body;
         const empresa = new Empresa({ nombre, nivelImpacto, yearsTrayectoria, categoriaEmpresarial });
 
-        await nuevaEmpresa.save();
+        await empresa.save();
         res.status(200).json({
             empresa
         });
 };
+
+export const getAllEmpresas = async () => {
+    try {
+      const empresas = await Empresa.findAll();
+      return empresas;
+    } catch (error) {
+      console.error('Error al obtener empresas:', error);
+      throw error;
+    }
+};
+
+export const obtenerEmpresas = async () => {
+    try {
+      const empresas = await Empresa.find();
+      return empresas; 
+    } catch (error) {
+      console.error('Error al obtener las empresas:', error);
+      throw error;
+    }
+};
+
+
+export const generarReporteExcel = async (empresas) => {
+    try {
+      const directorio = process.env.ARCHIVOS_DIR;
+  
+      if (!directorio) {
+        throw new Error('La variable de entorno ARCHIVOS_DIR no está definida');
+      }
+      if (!fs.existsSync(directorio)) {
+        fs.mkdirSync(directorio, { recursive: true });
+      }
+  
+      const workbook = new ExcelJS.Workbook();
+      const worksheet = workbook.addWorksheet('Empresas');
+  
+      worksheet.addRow(['Nombre', 'Nivel de Impacto', 'Años de Trayectoria', 'Categoría Empresarial']);
+      
+      empresas.forEach((empresa) => {
+        worksheet.addRow([
+          empresa.nombre,
+          empresa.nivelImpacto,
+          empresa.yearsTrayectoria,
+          empresa.categoriaEmpresarial
+        ]);
+      });
+  
+      const filePath = path.join(directorio, 'reporte_empresas.xlsx');
+      await workbook.xlsx.writeFile(filePath);
+  
+      return filePath;
+    } catch (error) {
+      console.error('Error al generar el reporte de Excel:', error);
+      throw error;
+    }
+  };
+
--- src/empresa/empresa.routes.js
+++ src/empresa/empresa.routes.js
@@ -1,36 +1,52 @@
-import { Router } from "express";
+import { Router, response, request } from "express";
 import { check } from "express-validator";
 import {
     getEmpresas,
-    crearEmpresa
+    crearEmpresa,
+    obtenerEmpresas,
+    generarReporteExcel
 } from "./empresa.controller.js";
-//import {
-//  existeEmpresaById
-//} from "../helpers/db-validator.js";
 import { validarCampos } from "../middlewares/validar-campos.js";
 
 const router = Router();
 
 router.get("/", getEmpresas);
 
-//router.get(
-//  "/:id",
-//  [
-//    check("id", "No es un ID válido").isMongoId(),
-//    check("id").custom(existeEmpresaById),
-//    validarCampos,
-//  ],
-//  getUserById
-//);
 
 router.post(
     '/',
     [
       check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-      check('nivelImpacto').optional().isString(), // Validación opcional para nivelImpacto
-      check('yearsTrayectoria').optional().isInt(), // Validación opcional para anosTrayectoria
-      check('categoriaEmpresarial').optional().isString(), // Validación opcional para categoriaEmpresarial
-      validarCampos, // Middleware para validar los campos de la solicitud
-    ],
-    crearEmpresa // Controlador para crear una nueva empresa
-  );
+      check('nivelImpacto').optional().isString(), 
+      check('yearsTrayectoria').optional().isInt(), 
+      check('categoriaEmpresarial').optional().isString(),
+      validarCampos, 
+    ], crearEmpresa);
+
+    router.post('/generar-reporte', async (req, res) => {
+        try {
+          // Obtener las empresas
+          const empresas = await obtenerEmpresas();
+      
+          const directorioReportes = './reportes';
+          // Llamada a la función para generar el reporte
+          const filePath = await generarReporteExcel(empresas, directorioReportes);
+      
+          // Envía el archivo Excel como respuesta
+          res.download(filePath, 'reporte_empresas.xlsx', (err) => {
+            if (err) {
+              console.error('Error al enviar el archivo:', err);
+              res.status(500).json({ mensaje: 'Error al enviar el archivo' });
+            } else {
+              console.log('Archivo enviado correctamente');
+            }
+          });
+      
+        } catch (error) {
+          // Manejo de errores
+          console.error('Error al generar el reporte de empresas:', error);
+          res.status(500).json({ mensaje: 'Error al generar el reporte de empresas' });
+        }
+      });
+
+export default router;
